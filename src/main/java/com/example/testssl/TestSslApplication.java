735dbc7902f709f076ebb1eb5b7c47a24abf6562
Behruzbekolimov <olimovbehruzbek@gmail.com>
2023-02-28 11:24:57 +0500

    Initial commit

---
+++ src/main/java/com/example/testssl/TestSslApplication.java
@@ -0,0 +1,13 @@
+package com.example.testssl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSslApplication.class, args);
+    }
+
+}
